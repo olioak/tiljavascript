609b619f79b5bb3d01a3b87d4df90794a0e0bc12
Oli Oak <61263618+olioak@users.noreply.github.com>
2020-05-21 15:36:46 +0200

    Describe it is online

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>This is cool! Sort of.</p>
+    <p>This is cool! Sort of. And it is online!</p>
   </Layout>
 )
 
