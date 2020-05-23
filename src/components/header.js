75ad71229b7413cdd7289761d024e05689c20d28
Oli Oak <61263618+olioak@users.noreply.github.com>
2020-05-23 19:17:50 +0200

    Go back to "normal" theme use

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,37 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useContext } from "react"
-import styled, { ThemeContext } from "styled-components"
+import React from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.header`
+  background: ${p => p.theme.palette.gray[800]};
+  margin-bottom: ${p => p.theme.spacing[8]};
+  padding-top: ${p => p.theme.spacing[2]};
+  padding-bottom: ${p => p.theme.spacing[2]};
+  text-align: center;
+`
+
+const Inner = styled.div`
+  margin: 0 auto;
+  max-width: ${p => p.theme.screens.sm};
+`
+
+const StyledLink = styled(Link)`
+  color: ${p => p.theme.palette.yellow[400]};
+  text-transform: uppercase;
+  text-decoration: none;
+  &:hover {
+    color: ${p => p.theme.palette.gray[100]};
+  }
+`
+
+const Title = styled.h1`
+  font-size: ${p => p.theme.fontSize["3xl"]};
+  margin: 0;
+  padding: 16px;
+`
 
 const Header = ({ siteTitle }) => {
-  const theme = useContext(ThemeContext)
-  const { palette, spacing, screens, fontSize } = theme
-
-  const Wrapper = styled.header`
-    background: ${palette.gray[800]};
-    margin-bottom: ${spacing[8]};
-    padding-top: ${spacing[2]};
-    padding-bottom: ${spacing[2]};
-    text-align: center;
-  `
-
-  const Inner = styled.div`
-    margin: 0 auto;
-    max-width: ${screens.sm};
-  `
-
-  const StyledLink = styled(Link)`
-    color: ${palette.red[400]};
-    text-transform: uppercase;
-    text-decoration: none;
-    &:hover {
-      color: ${palette.gray[100]};
-    }
-  `
-
-  const Title = styled.h1`
-    font-size: ${fontSize["3xl"]};
-    margin: 0;
-    padding: 16px;
-  `
-
   return (
     <Wrapper>
       <Inner>
