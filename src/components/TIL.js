27cbd9868b7f6c77ec4f0028bc37dcafa9677a27
Oli Oak <61263618+olioak@users.noreply.github.com>
2020-05-24 22:32:01 +0200

    Add favicon

--- src/components/TIL.js
+++ src/components/TIL.js
@@ -143,8 +143,8 @@ const TIL = ({ data }) => {
             <Chevron open={showReferences}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="12"
-                height="12"
+                width="10"
+                height="10"
                 fill="currentColor"
                 viewBox="0 0 24 24"
               >
