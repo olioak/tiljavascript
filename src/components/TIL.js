2744a749b1c0d37ac34e1ca1324ccfdc7b8e6cba
Oli Oak <61263618+olioak@users.noreply.github.com>
2020-05-24 20:53:24 +0200

    Toggle references

--- src/components/TIL.js
+++ src/components/TIL.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link } from "gatsby"
 import styled, { css } from "styled-components"
@@ -57,7 +57,7 @@ const RarityTag = styled.span`
   }}
 `
 
-const Tag = styled.span`
+const Tag = styled.div`
   ${({ theme: { palette, spacing, borderRadius, fontSize } }) => css`
     background-color: ${palette.white};
     border: 1px solid ${palette.gray[400]};
@@ -65,13 +65,50 @@ const Tag = styled.span`
     padding: 0 ${spacing[2]};
     margin: ${spacing[1]};
     border-radius: ${borderRadius.lg};
-    display: inline-block;
+
     font-size: ${fontSize.sm};
   `}
 `
 
+const ReferencesContainer = styled.div`
+  padding: ${p => p.theme.spacing[4]};
+`
+
+const ReferencesToggle = styled.button`
+  color: ${p => p.theme.palette.blue[600]};
+  background-color: ${p => p.theme.palette.gray[200]};
+  text-align: right;
+  border: none;
+  flex: 1;
+  padding: 0 ${p => p.theme.spacing[2]};
+  &:focus {
+    outline: none; /* not happy with this (a11y), but not sure how to improve it*/    
+  }
+`
+
+const TagsAndToggle = styled.div`
+  display: flex;
+  align-items: center;
+  padding: ${p => p.theme.spacing[2]};
+`
+
+const Chevron = styled.span`
+  ${props => {
+    const rotation = props.open ? "90deg" : "180deg"
+    return css`
+      transform: rotate(${rotation});
+      display: inline-block;
+      cursor: pointer;
+      padding: ${p => p.theme.spacing[1]};
+    `
+  }}
+`
+
 const TIL = ({ data }) => {
   const { frontmatter, body, fields } = data
+  const [showReferences, setShowReferences] = useState(false)
+
+  const toggle = () => setShowReferences(prev => !prev)
 
   return (
     <Wrapper>
@@ -82,19 +119,41 @@ const TIL = ({ data }) => {
         <FixedMarkdown>
           <MDXRenderer>{body}</MDXRenderer>
         </FixedMarkdown>
-        <RarityTag rarity={frontmatter.rarity}>{frontmatter.rarity}</RarityTag>
-        {frontmatter.categories.map((category, idx) => {
-          return <Tag key={idx}>{category}</Tag>
-        })}
 
-        {/* 
-        {frontmatter.references.map((ref, idx) => {
-          return (
-            <span key={idx}>
-              {ref.split(":")[0]} - {ref.split(":")[1]}
-            </span>
-          )
-        })} */}
+        <TagsAndToggle>
+          <RarityTag rarity={frontmatter.rarity}>
+            {frontmatter.rarity}
+          </RarityTag>
+          {frontmatter.categories.map((category, idx) => {
+            return <Tag key={idx}>{category}</Tag>
+          })}
+
+          <ReferencesToggle onClick={toggle}>
+            <span>references</span>
+            <Chevron open={showReferences}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="12"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+              </svg>
+            </Chevron>
+          </ReferencesToggle>
+        </TagsAndToggle>
+        {showReferences && (
+          <ReferencesContainer>
+            {frontmatter.references.map((ref, idx) => {
+              return (
+                <div key={idx}>
+                  {ref.split(":")[0]} - {ref.split(":")[1]}
+                </div>
+              )
+            })}
+          </ReferencesContainer>
+        )}
       </Body>
     </Wrapper>
   )
