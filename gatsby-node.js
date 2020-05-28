c058b0ee630b01f09b5b6092745a472d08032ee4
Oli Oak <61263618+olioak@users.noreply.github.com>
2020-05-28 11:31:27 +0200

    Get slug from title

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,15 +4,34 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`);
+const kebabCase = require(`lodash.kebabcase`);
 
-const { createFilePath } = require(`gatsby-source-filesystem`)
+/**
+ * Converts a title to a slug.
+ * It lowercases it first or it will convert JavaScript to java-script.
+ * There is no need to deburr.
+ * @param {*} title
+ */
+const titleToSlug = title => kebabCase(title.toLowerCase());
 
-const path = require(`path`)
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `Mdx`) {
+    // const value = createFilePath({ node, getNode });
+    const value = titleToSlug(node.frontmatter.title);
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    });
+  }
+};
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
-  const tilTemplate = path.resolve("src/templates/tilTemplate.js")
+  const { createPage } = actions;
+  const tilTemplate = path.resolve("src/templates/tilTemplate.js");
 
   return graphql(`
     {
@@ -29,10 +48,11 @@ exports.createPages = ({ actions, graphql }) => {
     }
   `).then(result => {
     if (result.errors) {
-      throw result.errors
+      throw result.errors;
     }
 
-    const tils = result.data.allMdx.nodes
+    const tils = result.data.allMdx.nodes;
+    console.log(tils[0].fields.slug);
 
     // create page for each mdx file
     tils.forEach(til => {
@@ -42,19 +62,7 @@ exports.createPages = ({ actions, graphql }) => {
         context: {
           slug: til.fields.slug,
         },
-      })
-    })
-  })
-}
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `Mdx`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
+      });
+    });
+  });
+};
