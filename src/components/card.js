b22c2d3ac368e9ad64775f7e22e5adf982ac8a88
Oli Oak <61263618+olioak@users.noreply.github.com>
2020-09-01 13:26:20 +0200

    Make background lighter.

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Card = styled.div`
-  background: ${p => p.theme.palette.gray[200]};
+  background: ${p => p.theme.palette.gray[100]};
   border-radius: ${p => p.theme.borderRadius.md};
   overflow: hidden; /* clip the border radius */
   margin-bottom: ${p => p.theme.spacing[8]};
